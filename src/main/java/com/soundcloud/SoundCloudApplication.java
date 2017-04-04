96210d504851295d4d78f0822e06ef53a567bb1d
Wnbj <wnbkrumov@gmail.com>
2017-04-05 02:38:52 +0300

    init

---
+++ src/main/java/com/soundcloud/SoundCloudApplication.java
@@ -0,0 +1,12 @@
+package com.soundcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundCloudApplication.class, args);
+	}
+}
