33526fddbbae774b4691eba6f14459cd1beda7b5
Rozalia <rosalia.marinova@gmail.com>
2017-04-17 01:01:46 +0300

    -

--- src/main/java/com/db/UserDAO.java
+++ src/main/java/com/db/UserDAO.java
@@ -11,13 +11,6 @@ import java.util.Set;
 
 import com.model.User;
 
-import DB.DatabaseManager;
-import Exceptions.InvalidEmailException;
-import Exceptions.InvalidMobileNumberException;
-import Exceptions.InvalidNameException;
-import Exceptions.InvalidPasswordException;
-import Exceptions.InvalidUserLoginException;
-import Exceptions.InvalidUserNameException;
 
 public class UserDAO {
 	
